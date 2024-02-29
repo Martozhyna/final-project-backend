e7b7e873742ec5a88f4d7ef2829d74a815da362a
Martozhyna <mybonanzaluck@gmail.com>
2024-02-29 19:38:52 +0200

    fix pagination`s warning

--- apps/groups/models.py
+++ apps/groups/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class GroupsModel(models.Model):
     class Meta:
         db_table = 'groups'
+        ordering = ('title',)
 
     title = models.CharField(max_length=20, unique=True)
