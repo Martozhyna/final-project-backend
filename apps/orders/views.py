50ff12f3c70da7834843d6dcde12045dba7e945b
Martozhyna <mybonanzaluck@gmail.com>
2024-03-31 17:24:46 +0300

    fix excel method

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -8,6 +8,8 @@ from rest_framework.response import Response
 from django_filters import rest_framework as filters
 import pandas as pd
 from django.http import HttpResponse
+from django.db.models import Q
+from datetime import datetime
 
 from apps.comments.serializers import CommentSerializer
 from apps.groups.models import GroupsModel
@@ -157,14 +159,65 @@ class OrdersExcelTable(ListAPIView):
                  get exel tables
     """
     queryset = OrdersModel.objects.all()
+    filterset_class = OrderFilter
+    filter_backends = (OrderingFilter, filters.DjangoFilterBackend,)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # Отримати параметри фільтрації з URL
+        name = self.request.query_params.get('name')
+        surname = self.request.query_params.get('surname')
+        email = self.request.query_params.get('email')
+        phone = self.request.query_params.get('phone')
+        age = self.request.query_params.get('age')
+        course = self.request.query_params.get('course')
+        course_type = self.request.query_params.get('course_type')
+        course_format = self.request.query_params.get('course_format')
+        statuses = self.request.query_params.get('status')
+        group = self.request.query_params.get('group')
+        start_data = self.request.query_params.get('start_date')
+        end_data = self.request.query_params.get('end_date')
+        manager = self.request.query_params.get('manager')
+
+        # Застосувати фільтри до queryset
+        if course_type:
+            queryset = queryset.filter(course_type=course_type)
+        if course_format:
+            queryset = queryset.filter(course_format=course_format)
+        if name:
+            queryset = queryset.filter(Q(name__icontains=name))
+        if surname:
+            queryset = queryset.filter(Q(surname__icontains=surname))
+        if email:
+            queryset = queryset.filter(Q(email__icontains=email))
+        if phone:
+            queryset = queryset.filter(Q(phone__icontains=phone))
+        if age:
+            queryset = queryset.filter(Q(age__icontains=age))
+        if course:
+            queryset = queryset.filter(course=course)
+        if statuses:
+            queryset = queryset.filter(status=statuses)
+        if group:
+            queryset = queryset.filter(group=group)
+        if start_data:
+            start_date = datetime.strptime(start_data, '%Y-%m-%d')
+            queryset = queryset.filter(created_at__gt=start_date)
+        if end_data:
+            end_date = datetime.strptime(end_data, '%Y-%m-%d')
+            queryset = queryset.filter(created_at__lte=end_date)
+        if manager:
+            queryset = queryset.filter(manager=manager)
+
+        return queryset
 
     def get(self, *args, **kwargs):
-        orders = OrdersModel.objects.all()  # отримуємо всі заявки з бази даних
+        orders = self.get_queryset()  # фільтруємо заявки на основі параметрів
         serializer = OrdersExelSerializer(instance=orders, many=True)  # перетворюємо їх у формат JSON
         data = serializer.data  # усі змінені замовлення
-        df = pd.DataFrame(data)  # робимо exel табличку
-        temp_dir = tempfile.mkdtemp()  # cтворюємо тимчасовий каталог для збереження таблички
-        excel_file_path = os.path.join(temp_dir, 'excel_file.xlsx')  # шлях до каталога
+        df = pd.DataFrame(data)  # робимо excel табличку
+        temp_dir = tempfile.mkdtemp()  # створюємо тимчасовий каталог для збереження таблички
+        excel_file_path = os.path.join(temp_dir, 'excel_file.xlsx')  # шлях до каталогу
         df.to_excel(excel_file_path, index=False)  # записуємо дані в табличку без індексів
         with open(excel_file_path, 'rb') as excel_file:
             response = HttpResponse(excel_file.read(),
