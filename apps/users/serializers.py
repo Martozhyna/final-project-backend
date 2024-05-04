c04799c7a315045c2d2ce21b6162682cba3a8f2b
Martozhyna <mybonanzaluck@gmail.com>
2024-05-04 21:14:01 +0300

    changed password create

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,3 +1,6 @@
+import secrets
+import string
+
 from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
@@ -9,7 +12,6 @@ UserModel = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
-
 
     class Meta:
         model = UserModel
@@ -25,8 +27,13 @@ class UserSerializer(serializers.ModelSerializer):
     #         raise serializers.ValidationError("Passwords do not match")
     #     return data
 
+    def generate_random_password(self, length=12):
+        alphabet = string.ascii_letters + string.digits
+        password = ''.join(secrets.choice(alphabet) for _ in range(length))
+        return password
+
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
+        password = self.generate_random_password()
         user = UserModel.objects.create(**validated_data)
         user.set_password(password)
         user.save()
